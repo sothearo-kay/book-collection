22b7abb17117df887090ed4229241192d82f7d01
sothearo-kay <sothearo.kay@techostartup.center>
2025-05-07 15:13:24 +0700

    fix(server): resolve accessing cookies after redirects (network)

--- src/lib/server/session.ts
+++ src/lib/server/session.ts
@@ -74,13 +74,19 @@ export async function invalidateSession(sessionId: string) {
 
 export function setSessionTokenCookie(event: RequestEvent, token: string, expiresAt: Date) {
 	event.cookies.set(sessionCookieName, token, {
-		expires: expiresAt,
-		path: '/'
+		path: '/',
+		httpOnly: true,
+		secure: import.meta.env.PROD,
+		sameSite: 'lax',
+		expires: expiresAt
 	});
 }
 
 export function deleteSessionTokenCookie(event: RequestEvent) {
 	event.cookies.delete(sessionCookieName, {
-		path: '/'
+		path: '/',
+		httpOnly: true,
+		secure: import.meta.env.PROD,
+		sameSite: 'lax'
 	});
 }
