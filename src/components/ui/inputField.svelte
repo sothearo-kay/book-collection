97f4458c770222a427202a6576ba373b6ce32a9f
sothearo-kay <sothearo.kay@techostartup.center>
2025-05-12 17:12:50 +0700

    style: auto fill styles

--- src/components/ui/inputField.svelte
+++ src/components/ui/inputField.svelte
@@ -15,13 +15,11 @@
 		<label for={id} class="ms-2 mb-2 text-sm font-medium">{label}</label>
 	{/if}
 	<div class="relative">
+		<input {id} {...inputProps} class="peer w-full rounded-lg border py-2 pr-4 pl-10 text-sm" />
 		{#if Icon}
-			<Icon class="absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2" />
+			<Icon
+				class="absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 dark:peer-autofill:text-black"
+			/>
 		{/if}
-		<input
-			{id}
-			{...inputProps}
-			class="w-full rounded-lg border bg-transparent py-2 pr-4 pl-10 text-sm"
-		/>
 	</div>
 </div>
