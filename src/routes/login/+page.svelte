0207deaf5c28a9cbf42eab63cc4f6428eb071c5e
sothearo-kay <sothearo.kay@techostartup.center>
2025-05-13 08:33:03 +0700

    style: add color theme for error

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -136,7 +136,8 @@
 					class={[
 						'flex items-center gap-x-3 rounded-lg border px-4 py-2',
 						'md:absolute md:inset-x-0 md:-bottom-4 md:translate-y-full',
-						'border-red-300 bg-red-100 text-red-800'
+						'border-error-border bg-error-bg text-error-text',
+						'dark:border-error-border-dark dark:bg-error-bg-dark dark:text-error-text-dark'
 					]}
 				>
 					<BadgeX class="h-5 w-5" />
